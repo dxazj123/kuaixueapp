3633fbe9bd14e850b6d5c040fc5f66f5b0fc73fc
HillelDu <756477101@qq.com>
2019-09-17 11:06:40 +0800

    thrid commit

--- KUAIXUEPHP/Lib/Core/Application.class.php
+++ KUAIXUEPHP/Lib/Core/Application.class.php
@@ -1,6 +1,66 @@
 <?php 
  	final class Application{
  		public static function run(){
- 			p(1);
+ 			self::_init();
+ 			self::_set_url();
+ 			spl_autoload_register(array(__CLASS__,'_autoload'));
+ 			self::_create_demo();
  		}
+ 		/**
+ 		 * 创建默认控制器
+ 		 */
+ 		private static function _create_demo(){
+ 			$path = APP_CONTROLLER_PATH . '/IndexController.class.php';
+ 			$str = <<<str
+<?php
+	class IndexController{
+		public function index(){
+			echo 'OK';
+		}
+	}
+?> 			
+str;
+			is_file($path) || file_put_contents($path,$str);
+ 		}
+ 		/**
+ 		 * 自动载入功能
+ 		 */
+ 		private static function _autoload($className){
+ 			echo $className;
+ 		}
+ 		/**
+ 		 * 设置外部路径
+ 		 */
+ 		private static function _set_url(){
+ 			// p($_SERVER);
+ 			$path = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
+ 			$path = str_replace('\\','/', $path);
+ 			define('__APP__', $path);
+ 			define('__ROOT__',dirname(__APP__));
+ 			define('__TPL__',__ROOT__ . '/' . APP_NAME . '/Tpl');
+ 			define('__PUBLICE__',__TPL__ . '/Public');
+ 		}
+ 		/**
+ 		 * 初始化框架
+ 		 */
+ 		private static function _init(){
+ 			//加载配置项
+ 			C(include CONFIG_PATH . '/config.php');
+ 			$userPath = APP_CONFIG_PATH . '/config.php';
+
+ 			$userConfig = <<<str
+<?php
+return array(
+	//配置项=> 配置值
+	);	
+?>		
+str;
+			is_file($userPath) || file_put_contents($userPath,$userConfig);
+			//加载用户配置项
+			C(include $userPath);
+			//设置默认时区
+			date_default_timezone_set(C('DEFAULT_TIME_ZONE'));
+			C('SESSION_AUTO_START') && session_start();
+ 		}
+ 		
  	}
--- KUAIXUEPHP/Lib/Function/function.php
+++ KUAIXUEPHP/Lib/Function/function.php
@@ -4,4 +4,33 @@
 		print_r($arr);
 		echo "</pre>";	
 	}
- ?>
+	//1.加载配置项
+	//C($sysConfig) C($userConfig)
+	//2.读取配置项
+	//C('CODE_LEN')
+	//3.临时动态改变配置项
+	//C('CODE_LEN',20)
+	//4.C();
+ 	function C($var = NULL, $value = NULL){
+ 		static $config = array();
+ 		//加载配置项
+ 		if(is_array($var)){
+ 			$config = array_merge($config,array_change_key_case($var,CASE_UPPER));
+ 			return;
+ 		}
+ 		//读取或者动态改变配置项
+ 		if(is_string($var)){
+ 			$var = strtoupper($var);
+
+ 			if(!is_null($value)){
+ 				$config[$var] = $value;
+ 				return;
+ 			}
+
+ 			return isset($config[$var]) ? $config[$var] : NULL;
+ 		}
+
+ 		if(is_null($var) && is_null($value)){
+ 			return $config;
+ 		}
+ 	}
